ca41b06220408fe66ce6150fb0b679ab9f453cba
CBarnes99 <calumbarnes@hotmail.com>
2025-10-16 14:56:35 +0100

    Add Enemy Spawner
    Added a spawner that can spawn in enemies that are linked to an array in the BP editor

---
+++ Source/TowerDefense/EnemySpawner.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawner.h"
+
+// Sets default values
+AEnemySpawner::AEnemySpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	spawnAmount = EnemyCharacters.Num();
+
+	for (int i = 0; i < spawnAmount; i++)
+	{
+		spawnEnemy();
+	}
+}
+
+// Called every frame
+void AEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+//Spawns the Enemy Actors from the Array of characters that are assigned in the BP Editor
+AActor* AEnemySpawner::spawnEnemy()
+{
+	FActorSpawnParameters spawnParams;
+	AActor* spawnedEnemy = GetWorld()->SpawnActor<AEnemyCharacterBase>(EnemyCharacters[0].Get(), this->GetActorLocation(), this->GetActorRotation(), spawnParams);
+	return spawnedEnemy;
+
+	//GetWorld()->SpawnActor<AEnemyCharacterBase>(this->GetActorLocation(), this->GetActorRotation(), spawnParams);
+}
+
---
+++ Source/TowerDefense/EnemySpawner.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyCharacterBase.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Spawner")
+	TArray<TSubclassOf<AEnemyCharacterBase>> EnemyCharacters;
+
+	int spawnAmount;
+	
+	AActor* spawnEnemy();
+};
