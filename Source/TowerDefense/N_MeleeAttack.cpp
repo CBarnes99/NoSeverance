9ff73631c232d0ce6d97b3c4385ecb81fc043dbc
CBarnes99 <calumbarnes@hotmail.com>
2026-01-12 15:26:55 +0000

    ADD Enemy Attack Animation
    CB - The Enemies now play and attack animation and can now damage the player

---
+++ Source/TowerDefense/N_MeleeAttack.cpp
@@ -0,0 +1,59 @@
+#include "N_MeleeAttack.h"
+#include "EnemyCharacterBase.h"
+#include "PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void UN_MeleeAttack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+
+	if (!MeshComp)
+	{
+		UE_LOG(LogTemp, Error, TEXT("NO MESH WITHIN - %s"), *this->GetName());
+		return;
+	}
+
+	if (attackSocketName.IsNone() || !MeshComp->DoesSocketExist(attackSocketName))
+	{
+		UE_LOG(LogTemp, Error, TEXT("ATTACK SOCKET NAME NOT SET CORRECTLY WITHIN - %s, %s, %s"), *MeshComp->GetOwner()->GetName(), *Animation->GetName(), *this->GetName());
+		return;
+	}
+
+	AEnemyCharacterBase* enemy = Cast<AEnemyCharacterBase>(MeshComp->GetOwner());
+
+	if (!enemy)
+	{
+		UE_LOG(LogTemp, Error, TEXT("CASTED TO ENEMY FAILED WITHIN - %s"), *this->GetName());
+		return;
+	}
+
+	if (!enemy->HasAuthority())
+	{
+		UE_LOG(LogTemp, Error, TEXT("ENEMY DOEST NOT HAVE AUTHORITY WITHIN - %s"), *this->GetName());
+		return;
+	}
+
+	if (traceRadius <= 1.f)
+	{
+		traceRadius = 50.f;
+	}
+
+	FVector boneLocation = MeshComp->GetSocketLocation(attackSocketName);
+
+	TArray<FHitResult> hitResults;
+	FCollisionShape sphereCollision = FCollisionShape::MakeSphere(traceRadius);
+
+	bool bHit = enemy->GetWorld()->SweepMultiByChannel(hitResults, boneLocation, boneLocation, FQuat::Identity, ECC_Pawn, sphereCollision);
+	DrawDebugSphere(GetWorld(), boneLocation, traceRadius, 12, FColor::Green, false, 1.f);
+
+
+	for (auto& hit : hitResults)
+	{
+		APlayerCharacter* player = Cast<APlayerCharacter>(hit.GetActor());
+		if (player)
+		{
+			UGameplayStatics::ApplyDamage(player, enemy->GetEnemyDamageAmount(), enemy->GetController(), enemy, nullptr);
+			UE_LOG(LogTemp, Warning, TEXT("Enemy Has Attacked Player!!"));
+			return;
+		}
+	}
+}
---
+++ Source/TowerDefense/N_MeleeAttack.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "N_MeleeAttack.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_API UN_MeleeAttack : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+	UPROPERTY(EditAnywhere)
+	FName attackSocketName;
+
+	UPROPERTY(EditAnywhere)
+	float traceRadius;
+
+};
